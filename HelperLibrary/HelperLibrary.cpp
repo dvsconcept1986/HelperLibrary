f9bbf6b964d452bd9e746956d4ad9971a9b6410a
dvsconcept1986 <ejfernandez@student.fullsail.edu>
2024-04-18 16:58:09 -0400

    Add project files.

---
+++ HelperLibrary/HelperLibrary.cpp
@@ -0,0 +1,11 @@
+// HelperLibrary.cpp : This file contains the 'main' function for testing the namespace functions that I
+// that i will declare in the Helper.h file.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
